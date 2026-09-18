67aa7793fafdc8fe310c31e1b5ead6d7102c19d4
DESKTOP-85RQR69\GIGABYTE <trungluat1305@gmail.com>
2026-09-19 00:05:58 +0700

    Add project files.

---
+++ 2611COMP101904-Lap-trinh-Windows/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace _2611COMP101904_Lap_trinh_Windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHoten = new System.Windows.Forms.TextBox();
+            this.txtNamsinh = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.radNam = new System.Windows.Forms.RadioButton();
+            this.radNu = new System.Windows.Forms.RadioButton();
+            this.cboKhoa = new System.Windows.Forms.ComboBox();
+            this.btnHienThi = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "THÔNG TIN SINH VIÊN";
+            // 
+            // txtHoten
+            // 
+            this.txtHoten.Location = new System.Drawing.Point(51, 59);
+            this.txtHoten.Name = "txtHoten";
+            this.txtHoten.Size = new System.Drawing.Size(100, 22);
+            this.txtHoten.TabIndex = 1;
+            this.txtHoten.Text = "Ho ten";
+            // 
+            // txtNamsinh
+            // 
+            this.txtNamsinh.Location = new System.Drawing.Point(51, 104);
+            this.txtNamsinh.Name = "txtNamsinh";
+            this.txtNamsinh.Size = new System.Drawing.Size(100, 22);
+            this.txtNamsinh.TabIndex = 2;
+            this.txtNamsinh.Text = "Nam sinh";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(51, 155);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 22);
+            this.txtEmail.TabIndex = 3;
+            this.txtEmail.Text = "Email";
+            // 
+            // radNam
+            // 
+            this.radNam.AutoSize = true;
+            this.radNam.Location = new System.Drawing.Point(67, 196);
+            this.radNam.Name = "radNam";
+            this.radNam.Size = new System.Drawing.Size(57, 20);
+            this.radNam.TabIndex = 4;
+            this.radNam.TabStop = true;
+            this.radNam.Text = "Nam";
+            this.radNam.UseVisualStyleBackColor = true;
+            // 
+            // radNu
+            // 
+            this.radNu.AutoSize = true;
+            this.radNu.Location = new System.Drawing.Point(188, 196);
+            this.radNu.Name = "radNu";
+            this.radNu.Size = new System.Drawing.Size(45, 20);
+            this.radNu.TabIndex = 5;
+            this.radNu.TabStop = true;
+            this.radNu.Text = "Nu";
+            this.radNu.UseVisualStyleBackColor = true;
+            // 
+            // cboKhoa
+            // 
+            this.cboKhoa.FormattingEnabled = true;
+            this.cboKhoa.Location = new System.Drawing.Point(51, 226);
+            this.cboKhoa.Name = "cboKhoa";
+            this.cboKhoa.Size = new System.Drawing.Size(121, 24);
+            this.cboKhoa.TabIndex = 6;
+            this.cboKhoa.Text = "Khoa";
+            // 
+            // btnHienThi
+            // 
+            this.btnHienThi.Location = new System.Drawing.Point(51, 277);
+            this.btnHienThi.Name = "btnHienThi";
+            this.btnHienThi.Size = new System.Drawing.Size(75, 23);
+            this.btnHienThi.TabIndex = 7;
+            this.btnHienThi.Text = "Hien Thi";
+            this.btnHienThi.UseVisualStyleBackColor = true;
+            this.btnHienThi.Click += new System.EventHandler(this.btnHienThi_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(243, 277);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 8;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(442, 277);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 9;
+            this.btnThoat.Text = "Thoat";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.AutoSize = true;
+            this.lblKetQua.Location = new System.Drawing.Point(48, 319);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(52, 16);
+            this.lblKetQua.TabIndex = 10;
+            this.lblKetQua.Text = "Ket qua";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnHienThi);
+            this.Controls.Add(this.cboKhoa);
+            this.Controls.Add(this.radNu);
+            this.Controls.Add(this.radNam);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNamsinh);
+            this.Controls.Add(this.txtHoten);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHoten;
+        private System.Windows.Forms.TextBox txtNamsinh;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.RadioButton radNam;
+        private System.Windows.Forms.RadioButton radNu;
+        private System.Windows.Forms.ComboBox cboKhoa;
+        private System.Windows.Forms.Button btnHienThi;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Label lblKetQua;
+    }
+}
+
---
+++ 2611COMP101904-Lap-trinh-Windows/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _2611COMP101904_Lap_trinh_Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cboKhoa.Items.Add("Công nghệ thông tin");
+            cboKhoa.Items.Add("Kinh tế");
+            cboKhoa.Items.Add("Quản trị kinh doanh");
+        }
+
+        private void btnHienThi_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtHoten.Text))
+            {
+                MessageBox.Show("Họ tên không được rỗng!");
+                txtHoten.Focus();
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtNamsinh.Text))
+            {
+                MessageBox.Show("Năm sinh không được rỗng!");
+                txtNamsinh.Focus();
+                return;
+            }
+            int namSinh;
+
+            if (!int.TryParse(txtNamsinh.Text, out namSinh))
+            {
+                MessageBox.Show("Năm sinh phải là số nguyên!");
+                txtNamsinh.Focus();
+                return;
+            }
+            int namHienTai = DateTime.Now.Year;
+
+            if (namSinh < 1900 || namSinh > namHienTai)
+            {
+                MessageBox.Show("Năm sinh phải từ 1900 đến " + namHienTai + "!");
+                txtNamsinh.Focus();
+                return;
+            }
+            if (!radNam.Checked && !radNu.Checked)
+            {
+                MessageBox.Show("Vui lòng chọn giới tính!");
+                return;
+            }
+            string gioiTinh;
+
+            if (radNam.Checked)
+                gioiTinh = "Nam";
+            else
+                gioiTinh = "Nữ";
+            int tuoi = DateTime.Now.Year - namSinh;
+            lblKetQua.Text =
+                "Họ tên: " + txtHoten.Text + "\n" +
+                "tuoi " + tuoi + "\n" +
+                "Email: " + txtEmail.Text + "\n" +
+                "Giới tính: " + gioiTinh + "\n" +
+                "Khoa: " + cboKhoa.Text;
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            txtHoten.Clear();
+            txtNamsinh.Clear();
+            txtEmail.Clear();
+
+            radNam.Checked = false;
+            radNu.Checked = false;
+
+            cboKhoa.SelectedIndex = -1;
+
+            lblKetQua.Text = "";
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+        "Bạn có muốn thoát không?",
+        "Xác nhận",
+        MessageBoxButtons.YesNo
+    );
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
